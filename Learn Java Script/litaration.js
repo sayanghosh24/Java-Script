f9f46fc9d8d8148e41283c572eba9d4b7295113b
Sayan Ghosh <159245402+sayanghosh24@users.noreply.github.com>
2024-07-27 20:00:04 +0530

    Update litaration.js

--- Learn Java Script/litaration.js
+++ Learn Java Script/litaration.js
@@ -44,15 +44,47 @@ for (let index = 0; index < myArray.length; index++) {
 //         break
 //     }
 //     console.log(`Value of i is ${i}`);
-    
+
 // }
 
 
 for (let i = 1; i <= 20; i++) {
     if (i == 10) {
-        console.log(`Detected 10`);
+        // console.log(`Detected 10`);
         continue
     }
-    console.log(`Value of i is ${i}`);
-    
+    // console.log(`Value of i is ${i}`);
+
 }
+
+
+
+
+
+// while & do while loop
+
+// while
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArr = [`a`, `b`, `c`, `d`]
+let arr = 0
+while (arr<myArr.length) {
+    // console.log(`value is ${myArr[arr]}`);
+    arr = arr + 1
+}
+
+// dowhile
+
+let score = 1
+do {
+    console.log(`score is ${score}`);    
+    score ++
+} while (score <=10);
+
+
+
