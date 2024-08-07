9b56035eae531165eeb4c063b1755fe0f7829906
Sayan Ghosh <159245402+sayanghosh24@users.noreply.github.com>
2024-08-07 19:02:03 +0530

    Add files via upload

---
+++ Learn Java Script/Events Code/script.js
@@ -0,0 +1,38 @@
+// // document.getElementById('owl').onclick = function (){
+// //     alert('Owl');
+// // }  // this is the not best approch
+
+// document.getElementById('owl').addEventListener('click', function(){
+//     alert('Owl');
+// } , false)  // this is the best approch
+
+
+// // attachEvent()
+// // jQuery - onEventListener ()
+
+
+// document.getElementById('images').addEventListener('click', function(e){
+//     console.log("clicked inside the ul");
+    
+// }, false) //  event propogetion
+
+
+// // type , timestamp , defaltPrevented
+// // target , toElement , srcElement , currentTarget
+// // clientX , clientY , pageX , pageY , screenX , screenY , offsetX
+// // altkey , ctrlkey , shiftkey , keyCode
+
+
+
+// document.getElementById('owl').addEventListener('click', function(e){
+//     console.log("Owl Clicked");
+//     e.stopPropagation()
+// }, false)  //  event propogetion
+
+
+// document.getElementById ('google').addEventListener('click', function(e){
+//     e.preventDefault()
+//     e.stopPropagation()  // stop bubling
+//     console.log("Google Clicked");
+// }, false)
+
